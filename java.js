66bc8bb11b6746b4d3006317ef3dc61b57607345
brandom-git <brandomvillanuevatrinidad@gmail.com>
2025-06-12 07:27:53 -0500

    Add files via upload

--- java.js
+++ java.js
@@ -1,4 +1,13 @@
-function alex(){
-  console.log("Bien");
+document.addEventListener('DOMContentLoaded', function() {
+  var ingresar = document.getElementById("ingresar");
+  
+  if (ingresar) {
+    ingresar.addEventListener('click', alerta);
+  } else {
+    console.error("No se encontró el elemento con el id 'ingresar'");
+  }
+});
+
+function alerta() {
+  window.location.href="csia.html";
 }
-alex();
